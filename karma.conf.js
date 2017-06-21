b3be18e72f61b588d38ded32b8e41acf228ce534
Dani Nebot <daniign@gmail.com>
2017-06-21 23:43:51 +0200

    test(karma conf): Bump karma version and simplify karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,14 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
-
 module.exports = function (config) {
   const configuration = {
     basePath: '.',
-    frameworks: ['jasmine', 'karma-typescript'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-typescript')
-    ],
+    frameworks: ['browserify', 'jasmine'],
     files: [
-      { pattern: 'src/**/*.ts', included: true, watched: true }
-    ],
-    exclude: [
+      { pattern: 'dist/**/*.js', included: true, watched: true }
     ],
     preprocessors: {
-      'src/**/*.ts': [ 'karma-typescript' ]
+      'dist/**/*.js': ['browserify']
     },
-    reporters: ['progress', 'karma-typescript'],
+    reporters: ['spec'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
